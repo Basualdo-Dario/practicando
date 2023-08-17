eee7ecfdf4c4dddefd39678e231303cc625cb463
Dario Basualdo <basualdodario057@gmail.com>
2023-08-17 11:44:11 -0300

    o

--- 3_JS/3_Ejercicios/2_Listado_de_Personas/Listado_de_Personas/js/app.js
+++ 3_JS/3_Ejercicios/2_Listado_de_Personas/Listado_de_Personas/js/app.js
@@ -5,6 +5,8 @@ class Persona{
         this._apellido = apellido;
         this._id = ++Persona.id;
     }
+    get id(){return this._id;}
+    set id(id){this._id = id;}
 
     get nombre(){return this._nombre;}
     set nombre(nombre){this._nombre = nombre}
@@ -37,13 +39,31 @@ class Personas{
             return lista ;
         }
 
+        eliminar(valor){
+            let con = 0;
+            for(let persona of this._personas){
+                con++;
+
+                if(parseInt(persona.id) === parseInt(valor)){
+                    
+                    //splice:el primer valor que se pone es el indice y el segundo es la cantidad de elementos a eliminar
+                    this._personas.splice(con,1);
+                }
+                else{
+                    continue;
+                }
+            }
+        }
+
 
 
 }
 
+/***********************************************************************/
 
 
 let lista = new Personas();
+
 function cargar(){
     
     let formulario = document.forms['persona'];
@@ -57,8 +77,19 @@ function cargar(){
         lista.agregar(new Persona(nombre.value,apellido.value));
         let ver = document.getElementById('lista');
         ver.innerHTML = lista.recorrer();
-
     }
 
+
+}
+
+
+function quitar(){
+
+    let formu = document.forms['eliminarper'];
+    let perEliminar = formu['eliminar'];
+
+    lista.eliminar(parseInt(perEliminar.value));
+    let ver = document.getElementById('lista');
+    ver.innerHTML = lista.recorrer();
 
 }
